0652c09421213c29a7def2804cf7be314d5ef3d6
Jamez Mayke <jamezmayke@gmail.com>
2022-02-16 10:29:57 -0300

    Adiconando funcionalidade para troca de imagens na sessão da compra da bicicleta

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ function mostrarPergunta(event) {
   const pergunta = event.currentTarget;
   const controls = pergunta.getAttribute('aria-controls');
   const resposta = document.getElementById(controls);
-  
+
   resposta.classList.toggle('ativa');
   const ativa = resposta.classList.contains('ativa')
   pergunta.setAttribute('aria-expanded', ativa);
@@ -37,3 +37,19 @@ function ativarPerguntas(pergunta) {
 }
 
 perguntas.forEach(ativarPerguntas);
+
+
+// Galeria das bicicletas
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width: 1000px)').matches;
+  if(media) galeriaContainer.prepend(img);
+}
+
+function clicarImagem(img) {
+  img.addEventListener('click', trocarImagem);
+}
+galeria.forEach(clicarImagem);
